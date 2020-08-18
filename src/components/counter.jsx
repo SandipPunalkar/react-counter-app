b01e85fffcb5f5c7b3fdc004c81fa378e8f030c1
Sandip Punalkar <sandippunalkar9@gmail.com>
2020-08-18 21:29:05 +0530

    Destructing arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
+    const { counter, OnIncrement, onDelete } = this.props;
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           className="btn btn-secondary btn-sm"
-          onClick={() => this.props.OnIncrement(this.props.counter)}
+          onClick={() => OnIncrement(counter)}
         >
           Increment
         </button>
         <button
           className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.counter.id)}
+          onClick={() => onDelete(counter.id)}
         >
           Delete
         </button>
